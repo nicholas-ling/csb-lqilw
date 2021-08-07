e93b694065b53e6de6de764878042072fa67494f
Code Sandbox <hello@codesandbox.io>
2021-08-07 15:29:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.19485d776cf72fcab73f9bdc89b442cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb42d3762ac76d68b66bf202b1c19e5",
+    "url": "https://nicholas-ling.github.io/csb-lqilw/index.html"
+  },
+  {
+    "revision": "2db362474067181a0663",
+    "url": "https://nicholas-ling.github.io/csb-lqilw/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7778107375426c2c4e5",
+    "url": "https://nicholas-ling.github.io/csb-lqilw/static/js/2.11f62b8d.chunk.js"
+  },
+  {
+    "revision": "2db362474067181a0663",
+    "url": "https://nicholas-ling.github.io/csb-lqilw/static/js/main.d761fe3d.chunk.js"
+  },
+  {
+    "revision": "2708f62b676ceb29ecf2",
+    "url": "https://nicholas-ling.github.io/csb-lqilw/static/js/runtime~main.35a54368.js"
+  }
+]);
